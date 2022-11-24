c07b63a7c30e93b0636ba7d7c37663cb2e98b9e9
Steve Younessi <63491724+styounessi@users.noreply.github.com>
2022-11-24 15:29:59 -0800

    Update excel_to_csv.py

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -1,11 +1,13 @@
-import pandas as pd
 import os
+import pandas as pd
 from glob import glob
 
+# Set folder that script is placed in as current directory
 abs = os.path.abspath(__file__)
 dir = os.path.dirname(abs)
 os.chdir(dir)
 
+# Bulk convert all xlsx files to csv while keeping original file names
 for excel_file in glob('*.xlsx'):
     print(excel_file)
     df = pd.read_excel(excel_file)
