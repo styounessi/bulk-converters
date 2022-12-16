6f0c095f3e94291139f4ca6dafa8432e49d277d4
Steve Younessi <63491724+styounessi@users.noreply.github.com>
2022-12-16 14:15:43 -0800

    Update multi_sheet_excel_to_csv.py

--- multi_sheet_excel_to_csv.py
+++ multi_sheet_excel_to_csv.py
@@ -7,9 +7,10 @@ abs = os.path.abspath(__file__)
 dir = os.path.dirname(abs)
 os.chdir(dir)
 
-# Bulk convert all xlsx sheets to csv and use sheet titles as new file names
+# Bulk convert all xlsx sheets to csv and use file name + sheet titles as new file names
 for excel_file in glob('*.xlsx'):
     print(excel_file)
     df = pd.read_excel(excel_file, sheet_name=None)
+    name = os.path.splitext(excel_file)[0]
     for key in df.keys():
-        df[key].to_csv('%s.csv' %key, index=None, header=True)
+        df[key].to_csv('%s_%s.csv' %(name, key), index=None, header=True)
